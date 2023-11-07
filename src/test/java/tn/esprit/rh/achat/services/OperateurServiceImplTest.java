c61f581042f76b024bf1fa3133ec9c0939439908
mayssaklay <mayssa.klay@esprit.tn>
2023-11-07 23:14:41 +0100

    docker file

---
+++ src/test/java/tn/esprit/rh/achat/services/OperateurServiceImplTest.java
@@ -0,0 +1,27 @@
+package tn.esprit.rh.achat.services;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class OperateurServiceImplTest {
+
+    @Test
+    void retrieveAllOperateurs() {
+    }
+
+    @Test
+    void addOperateur() {
+    }
+
+    @Test
+    void deleteOperateur() {
+    }
+
+    @Test
+    void updateOperateur() {
+    }
+
+    @Test
+    void retrieveOperateur() {
+    }
+}
